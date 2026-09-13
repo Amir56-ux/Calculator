039e58ed2e80528bef29c9d3e683f986eb595e36
Amir56-ux <amirlgnd009@gmail.com>
2026-09-13 00:45:07 -0700

    Initial calculator project

--- ind.js
+++ ind.js
@@ -1,2 +1,31 @@
 let currentvalue= ' ';
 document.getElementById('display').value = currentvalue;
+
+document.addEventListener("keydown", function(event) {
+
+    if (event.key >= "0" && event.key <= "9") {
+        currentvalue += event.key;
+    }
+
+    if (event.key === "+") {
+        currentvalue += "+";
+    }
+
+    if (event.key === "-") {
+        currentvalue += "-";
+    }
+
+    if (event.key === "*") {
+        currentvalue += "*";
+    }
+
+    if (event.key === "/") {
+        currentvalue += "/";
+    }
+
+    if (event.key === "Enter") {
+        currentvalue = eval(currentvalue);
+    }
+
+    document.getElementById("display").value = currentvalue;
+});
